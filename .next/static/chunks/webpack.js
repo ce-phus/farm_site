c9a7bb481e5f499f920f43d1147299db7eb41974
cephus <pnakitaren2@gmail.com>
2024-02-21 02:22:38 -0500

    task:delete some files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bfc8307ca27b5e11"; }
+/******/ 		__webpack_require__.h = function() { return "92986eb1c8bcf348"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
